ede79d063db0a77186b013b9e06db9f3e6d14e56
Neil Sorensen <nsorens@gmail.com>
2011-07-12 00:32:29 -0600

    Testing subtracting numbers (4) (failed)

--- romanconvert-test.rkt
+++ romanconvert-test.rkt
@@ -5,4 +5,5 @@
 (check-equal? (convert-to-roman 0) "" "Test 0")
 (check-equal? (convert-to-roman 1) "I" "Test 1")
 (check-equal? (convert-to-roman 2) "II" "Test 2")
+(check-equal? (convert-to-roman 4) "IV" "Test 4")
 (check-equal? (convert-to-roman 5) "V" "Test 5")
