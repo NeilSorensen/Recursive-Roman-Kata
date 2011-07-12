81f195ef87a994573eb13cfe5a5368daaa15d8ed
Neil Sorensen <nsorens@gmail.com>
2011-07-12 00:26:52 -0600

    Testing 5 (requires using different character than 'I') (failed)

--- romanconvert-test.rkt
+++ romanconvert-test.rkt
@@ -5,3 +5,4 @@
 (check-equal? (convert-to-roman 0) "" "Test 0")
 (check-equal? (convert-to-roman 1) "I" "Test 1")
 (check-equal? (convert-to-roman 2) "II" "Test 2")
+(check-equal? (convert-to-roman 5) "V" "Test 5")
