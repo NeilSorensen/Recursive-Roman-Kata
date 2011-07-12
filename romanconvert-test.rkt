554427c5c3a7e9073fc61ff9bf744f792f6a0c2a
Neil Sorensen <nsorens@gmail.com>
2011-07-12 00:40:32 -0600

    Adding several more test of larger, complicated numbers.  Discovered a typo

--- romanconvert-test.rkt
+++ romanconvert-test.rkt
@@ -7,3 +7,8 @@
 (check-equal? (convert-to-roman 2) "II" "Test 2")
 (check-equal? (convert-to-roman 4) "IV" "Test 4")
 (check-equal? (convert-to-roman 5) "V" "Test 5")
+(check-equal? (convert-to-roman 9) "IX" "Test 9")
+(check-equal? (convert-to-roman 12) "XII" "Test 12")
+(check-equal? (convert-to-roman 14) "XIV" "Test 14")
+(check-equal? (convert-to-roman 99) "XCIX" "Test 99")
+(check-equal? (convert-to-roman 1944) "MCMXLIV" "Test 1944")
