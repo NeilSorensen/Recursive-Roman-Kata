a79b92a8f068e4fefd6474883b2013cce0cd52e9
Neil Sorensen <nsorens@gmail.com>
2011-07-12 00:36:33 -0600

    Adding the subtracting characters to the primitives list

--- romanconvert.rkt
+++ romanconvert.rkt
@@ -1,11 +1,17 @@
 #lang racket
 
 (define roman-primitives (list (cons 1 #\I)
+                               (cons 4 (list #\I #\V))
                                (cons 5 #\V)
+                               (cons 9 (list #\I #\X))
                                (cons 10 #\X)
+                               (cons 40 (list #\X #\L))
                                (cons 50 #\L)
+                               (cons 90 (list #\X #\C))
                                (cons 100 #\C)
+                               (cons 400 (list #\C #\D))
                                (cons 500 #\D)
+                               (cons 900 (list #\X #\M))
                                (cons 1000 #\M)))
 
 (define (convert-to-roman value)
@@ -17,8 +23,11 @@
   (if (zero? value)
       '()
       (if (>= value (car (car romans)))
-          (append (list (cdr (car romans)))
-                  (convert-to-roman-rec (- value (car (car romans))) romans))
+          (if (list? (cdr (car romans)))
+              (append (cdr (car romans))
+                      (convert-to-roman-rec (- value (car (car romans))) romans))    
+              (append (list (cdr (car romans)))
+                      (convert-to-roman-rec (- value (car (car romans))) romans)))
           (convert-to-roman-rec value (cdr romans)))))
 
 (provide convert-to-roman)
